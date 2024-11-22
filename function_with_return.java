a42af6ca122b3903980c7739a080501c7615819f
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-22 11:24:00 +0600

    java method

--- function_with_return.java
+++ function_with_return.java
@@ -1,7 +1,13 @@
 public class function_with_return {
 
 
+    static void  myMethod(){
+        System.out.println("hello i am rokib");
+    }
     public static void main(String[] args) {
-        System.out.println("Hello Rokib");
+
+        String name = "Rokib";
+        System.out.println("Hello I am "+name+" i am 23 year's olde");
+        myMethod();
     }
 }
