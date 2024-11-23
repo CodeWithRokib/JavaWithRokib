9c5d03dbbbc030a6a21d0171e0a1849f8235d0ca
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-23 23:48:13 +0600

    multiple parameter pass

---
+++ ParameterPass_01.java
@@ -0,0 +1,11 @@
+public class ParameterPass_01 {
+    static void myMethod(String fname, int age) {
+        System.out.println(fname + " is " + age);
+    }
+
+    public static void main(String[] args) {
+        myMethod("Liam", 5);
+        myMethod("Jenny", 8);
+        myMethod("Anja", 31);
+    }
+}
