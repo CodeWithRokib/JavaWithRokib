177d30bc3ca0d2b4e0fd6fc1cd0db679dab1d3a3
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-23 23:34:28 +0600

    my function parameter type

---
+++ function_parameter.java
@@ -0,0 +1,10 @@
+public class function_parameter {
+
+    public static void myFunc(String name){
+        System.out.println("My name is: "+name);
+    }
+    public static void main(String[] args) {
+
+        myFunc("rokib");
+    }
+}
