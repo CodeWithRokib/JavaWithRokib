e86f3259758367e3f13211e0858c34b1319925ea
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-24 23:20:13 +0600

    check year in 2022

--- logicalParameter.java
+++ logicalParameter.java
@@ -1,5 +1,6 @@
 public class logicalParameter {
 
+
         static void checkAge(int age) {
 
             if (age < 18) {
@@ -10,9 +11,21 @@ public class logicalParameter {
             }
 
         }
+    static void checkYear(int year) {
 
+        if (year < 2000) {
+            System.out.println("Access denied - You are not old enough!");
+
+        } else {
+            System.out.println("Access granted - You are old enough!");
+        }
+
+    }
         public static void main(String[] args) {
-            checkAge(20);
+
+        checkAge(20);
+        checkYear(2022);
+
         }
 
 }
