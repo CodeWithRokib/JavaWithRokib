d3fdf52acaccb09d8003622aa6763ef7470dacc7
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-29 23:55:12 +0600

    parameter 03

--- Parameter_03.java
+++ Parameter_03.java
@@ -3,7 +3,12 @@ public class Parameter_03 {
         System.out.println("My name is  "+name);
     }
 
+    static void checkEmail(String mail){
+        System.out.println("My email Is: "+mail);
+    }
+
     public static void main(String[] args) {
         checkName("Rokib");
+        checkEmail("rokibulhasan@gmail.com");
     }
 }
