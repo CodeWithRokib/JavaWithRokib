dd32202e56fcf08def9ef026eb2972e88b96be59
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-11-29 23:11:32 +0600

    parameter

---
+++ Parameter_03.java
@@ -0,0 +1,9 @@
+public class Parameter_03 {
+    static void checkName(String name){
+        System.out.println("My name is  "+name);
+    }
+
+    public static void main(String[] args) {
+        checkName("Rokib");
+    }
+}
