114d4c96cd8866b4898cf1184ec9a53ea2ef7dcc
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-12-02 00:28:47 +0600

    string anagram

---
+++ StringAnagrame.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+
+public class StringAnagrame {
+    public static boolean areAnagrams(String str1, String str2) {
+        char[] arr1 = str1.toCharArray();
+        char[] arr2 = str2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        return Arrays.equals(arr1, arr2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(areAnagrams("listen", "silent"));
+        System.out.println(areAnagrams("hello", "world"));
+    }
+}
