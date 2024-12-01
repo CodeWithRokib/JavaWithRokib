d29c8ece121927be15dcde8cf1686a5fa69304a9
Rokibul Hasan Rokib <rokibulhasan018722@gmail.com>
2024-12-02 00:24:38 +0600

    count vowels

---
+++ CountVowels.java
@@ -0,0 +1,16 @@
+public class CountVowels {
+    public static int countVowels(String s) {
+        int count = 0;
+        String vowels = "aeiouAEIOU";
+        for (char c : s.toCharArray()) {
+            if (vowels.indexOf(c) != -1) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countVowels("hello world")); // Output: 3
+    }
+}
--- FizzBuzz.java
+++ FizzBuzz.java
@@ -12,7 +12,6 @@ public class FizzBuzz {
             }
         }
     }
-
     public static void main(String[] args) {
         fizzBuzz();
     }
