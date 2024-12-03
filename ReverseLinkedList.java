a6d94416b29fb1fe9c77b3aa9b1f123a40028696
rokibul-hasan-rokib <rokibulhasan018722@gmail.com>
2024-12-03 22:25:42 +0600

    reverse list

---
+++ ReverseLinkedList.java
@@ -0,0 +1,55 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+public class ReverseLinkedList {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+
+        while (curr != null) {
+            // Save next node
+            next = curr.next;
+            // Reverse the current node's pointer
+            curr.next = prev;
+            // Move prev and curr one step forward
+            prev = curr;
+            curr = next;
+        }
+        return prev; // New head of the reversed list
+    }
+
+    public static void printList(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.val + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Example: 1 -> 2 -> 3 -> 4 -> 5
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        // Reverse the linked list
+        head = reverseList(head);
+
+        System.out.println("Reversed List:");
+        printList(head);
+    }
+}
